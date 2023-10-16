9cfd0355f042338b63b04137a39049e761db1f60
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2023-10-16 11:24:22 +0200

    Stack-data done?

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,66 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+
+  //compare the two values to see it max-size it full
+    let contolStack = this.stackControl;
+    let maxAmounth = this.MAX_SIZE;
+
+    if (contolStack.length < maxAmounth){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+  // if stak empty - true <==> stak any items - false [>=1]
+    let emptyStack = this.stackControl;
+
+    if (emptyStack.length === 0){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    /*
+    (add to the end the item) 
+    -first check - canPush? 
+    -if cP true add the item to the stackControl 
+    -if cP flase "stack overflow" => error 'STACK_OVERFLOW'
+    */
+    //let pushStack = this.stackControl;
+    //let maxSize = this.MAX_SIZE;
+    let callback = this.canPush();
+
+    if (callback === true ) {
+      this.stackControl.push(item)
+    } else {
+      console.log('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // ... your code goes here
+    /*
+    - check stack items - call isEmpty 
+    - if isEmpty - flase remove last item stackControl
+    - if isEmpty - true stack UNderflow error message 'STACK_UNDERFLOW'
+    */
+    let callBack = this.isEmpty();
+
+    if (callBack === false) {
+      this.stackControl.pop()
+    } else {
+      console.log('STACK_UNDERFLOW')
+    }
+
   }
 
   display() {
-    // ... your code goes here
+    // return list of items
+  return this.stackControl
   }  
 }
 
